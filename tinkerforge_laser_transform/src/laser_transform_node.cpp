a11ed25c61b30f175291b04619e4b1320ab9d39d
gus484 <gustav484@web.de>
2015-03-24 07:45:22 +0100

    add imu convergence parameter

--- tinkerforge_laser_transform/src/laser_transform_node.cpp
+++ tinkerforge_laser_transform/src/laser_transform_node.cpp
@@ -12,9 +12,10 @@ int main (int argc, char **argv)
 {
   ros::init(argc, argv, "laser_transform");
   ros::NodeHandle n;
- 
+
   // Declare variables thar can be modified by launch file or command line.
   int rate;
+  int imu_convergence_speed;
   bool imu_msgs;
   bool gps_msgs;
   string pcl_in_topic;
@@ -22,7 +23,7 @@ int main (int argc, char **argv)
   string imu_topic;
   string gps_topic;
   //string odo_topic;
- 
+
   // Create a new LaserTransformer object.
   LaserTransform *node_lt = new LaserTransform();
 
@@ -31,15 +32,16 @@ int main (int argc, char **argv)
   // while using different parameters.
   ros::NodeHandle private_node_handle_("~");
   private_node_handle_.param("rate", rate, int(10));
-  private_node_handle_.param("imu_msgs", imu_msgs, bool(false));
+  private_node_handle_.param("imu_msgs", imu_msgs, bool(true));
   private_node_handle_.param("gps_msgs", gps_msgs, bool(false));
   private_node_handle_.param("pcl_in_topic", pcl_in_topic, string("/cloud"));
   private_node_handle_.param("pcl_out_topic", pcl_out_topic, string("/cloud_world"));
   private_node_handle_.param("imu_topic", imu_topic, string("/imu/data"));
   private_node_handle_.param("gps_topic", gps_topic, string("/gps/fix"));
+  private_node_handle_.param("imu_convergence_speed", imu_convergence_speed, int(20));
 
   // Create a subscriber
-  ros::Subscriber sub_message = n.subscribe(pcl_in_topic.c_str(), 1000, &LaserTransform::messageCallback, node_lt);
+  ros::Subscriber sub_message = n.subscribe(pcl_in_topic.c_str(), 1000, &LaserTransform::pclCallback, node_lt);
 
   // Create odometry subscriber
   ros::Subscriber sub_odometry = n.subscribe("/odometry/filtered", 50, &LaserTransform::odometryCallback, node_lt);
@@ -57,7 +59,7 @@ int main (int argc, char **argv)
 
   while (n.ok())
   {
-    node_lt->publishMessage(&pub_message);
+    node_lt->publishPclMessage(&pub_message);
     if (imu_msgs)
       node_lt->publishImuMessage(&imu_pub);
     if (gps_msgs)
